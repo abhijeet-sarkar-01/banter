59c932bfbb72185791fabc65c782b9ab18bcb146
abhijeet-sarkar-01 <abhijeet.sarkar7191@gmail.com>
2024-09-03 15:13:31 +0530

    Update the socket URL

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -16,7 +16,7 @@ export const SocketContextProvider = ({children}) => {
 
   useEffect(() => {
     if (authUser){
-      const socket = io("http://localhost:5000", {
+      const socket = io("https://banter-prod.onrender.com/", {
         query: {
           userId : authUser._id,
         }
